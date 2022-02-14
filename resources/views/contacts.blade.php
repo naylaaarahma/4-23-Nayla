c4ee0202ea2416ae942798d90409d892d44f6bb1
naylaaarahma <naylarahmamaulida1@gmail.com>
2022-02-14 20:30:16 +0700

    first commit

--- resources/views/contacts.blade.php
+++ resources/views/contacts.blade.php
@@ -4,20 +4,21 @@
 <div class="title-top mb-5">
     <h2 class="fw-bold">Contacts Us</h2>
 </div>
-<form>
+<form method="post" action="{{route('contacts.store')}}">
+  @csrf
   <div class="form-floating mb-3">
-    <input type="text" class="form-control" id="inputname" placeholder="Nama">
+    <input type="text" class="form-control" id="inputname" placeholder="Nama" name="name">
     <label for="inputname">Nama</label>
   </div>
   <div class="form-floating mb-3">
-     <input type="email" class="form-control" id="inputemail" placeholder="Email">
+     <input type="email" class="form-control" id="inputemail" placeholder="Email" name="email">
      <label for="inputemail" class="form-label">Email</label>
   </div>
   <div class="from-floating mb-3">
-    <textarea class="form-control" placeholder="Tinggalkan pesan disini!" id="floatingTextarea2" style="height: 100px" ></textarea>
-    <label for="floatingTextarea2">Pesan</label>
+    <textarea class="form-control" placeholder="Tinggalkan pesan disini!" id="floatingTextarea2" style="height: 100px"name="pesan"></textarea>
+   
   </div>
-  <button type="submit" class="btn btn-primary">Submit</button>
+  <button type="submit" class="btn btn-primary">Kirim</button>
 
 </form>
 @endsection
